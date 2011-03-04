3ab3038f1a12dd72f8fe922cd3d8d8b6bda0db0d
Spencer Bliven <sbliven@ucsd.edu>
2011-03-04 05:17:47 +0000

    Moved the align benchmark over from biojava.
    URL: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/biojava-live/trunk/biojava3-structure/src/test/java/org/biojava/bio/structure/align/benchmark
    Revision: 8748
    
    git-svn-id: svn+ssh://pdbcvs.rcsb.org/misc/pdb/svn/alignBenchmark/trunk@11764 c7567c6c-0293-4223-8362-9e6c12f08bf1

---
+++ src/main/java/org/rcsb/alignBenchmark/metrics/AlignmentLengthMetric.java
@@ -0,0 +1,57 @@
+package org.rcsb.alignBenchmark.metrics;
+
+
+import java.util.Map;
+
+import org.biojava.bio.structure.Atom;
+import org.biojava.bio.structure.align.model.AFPChain;
+import org.rcsb.alignBenchmark.metrics.Metric;
+import org.rcsb.alignBenchmark.MultipleAlignment;
+
+public abstract class AlignmentLengthMetric {
+	/**
+	 * Calculates the length of the reference alignment
+	 * @author Spencer Bliven
+	 *
+	 */
+	public static class Reference extends Metric {
+
+		@Override
+		public double calculate(MultipleAlignment reference, AFPChain align, Atom[] ca1, Atom[] ca2, Map<String, Object> metaData) {
+			return reference.size();
+		}
+
+		@Override
+		public String getName() {
+			return "Ref_len";
+		}
+
+		@Override
+		public String format(double result) {
+			return Integer.toString((int)result);
+		}
+	}
+	
+	/**
+	 * Calculates the length of the test alignment
+	 * @author Spencer Bliven
+	 *
+	 */
+	public static class Alignment extends Metric {
+
+		@Override
+		public double calculate(MultipleAlignment reference, AFPChain align, Atom[] ca1, Atom[] ca2, Map<String, Object> metaData) {
+			return (double)align.getAlnLength();
+		}
+
+		@Override
+		public String getName() {
+			return "Aln_len";
+		}
+		
+		@Override
+		public String format(double result) {
+			return Integer.toString((int)result);
+		}
+	}
+}
